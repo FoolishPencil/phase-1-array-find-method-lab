53d2a80666b7a3b84631246d3afb5b4c758433e0
daniel lance <poorfoolspencil@gmail.com>
2024-10-15 10:30:04 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function superbowlWin(record) {
+    const win = record.find(game => game.result === "W");
+    return win ? win.year : undefined;
+}
+
+const record = [
+    { year: "2014", result: "L" },
+    { year: "2015", result: "L" },
+    { year: "2016", result: "W" }
+];
+
+console.log(superbowlWin(record));
